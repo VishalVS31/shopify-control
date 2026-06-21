b8649adb1f413968de1ce5c277a3c95e3ec481bd
Vishal Sehgal <104946369+VishalVS31@users.noreply.github.com>
2026-06-21 16:26:50 +0530

    Add Vercel connection notification to site

---
+++ site-control.js
@@ -0,0 +1,16 @@
+document.body.insertAdjacentHTML(
+  "beforeend",
+  `
+  <div style="
+    position:fixed;
+    top:20px;
+    right:20px;
+    background:black;
+    color:white;
+    padding:20px;
+    z-index:999999999;
+  ">
+    Vercel Connected
+  </div>
+  `
+);
